1f94a01ab1bf3a5f40bc4a5e4a4d6557bc2aa498
ekirkegaard <kirkegaard.emily@gmail.com>
2018-04-18 11:39:14 -0700

    set up start button with event listener

--- js/app.js
+++ js/app.js
@@ -1,36 +1,58 @@
-// define global variables
-let startTime = 0;
-let endTime = 0;
-let countdown = 1500;
+// store time completed as global variables
+let workTimeCompleted = 0;
+let breakTimeCompleted = 0;
 
-// set up end time
-var countDownDate = new Date("Sep 5, 2018 15:37:25").getTime();
+// store pomodoros completed as global variables
+let pomsCompleted = 0;
+let breaksCompleted = 0;
 
-// update the countdown every 1 second
-var x = setInterval(function() {
 
-	// get current date and time
-  	var now = new Date().getTime();
+// arithmetic to turn ms to days/hours/minutes/seconds
+function interpretTime(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
 
-  	// find the distance between now an the countdown date
-  	var distance = countDownDate - now;
+// set up clock and update it every second
+function initializeClock(id, endtime) {
 
-  	// Time calculations for days, hours, minutes and seconds
-  	var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  var clock = document.getElementById(id);
+  var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
 
-  	// Display the result in the element with id="demo"
-  	document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  	+ minutes + "m " + seconds + "s ";
+  function updateClock() {
+    var t = interpretTime(endtime);
 
-  	// If the count down is finished, write some text
-  	if (distance < 0) {
-    	clearInterval(x);
-    	document.getElementById("demo").innerHTML = "EXPIRED";
-  	}
-}, 1000);
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
+
+// set up 25-minute countdown
+var deadline = new Date(Date.parse(new Date()) + 25 * 60 * 1000);
+
+// initializeClock('clockdiv', deadline);
 
 
 // set up work timer
@@ -49,21 +71,23 @@ var x = setInterval(function() {
 
 // }
 
-// add event listener for start button
+// add event listener for start/unpause button
 
 
 // set up event listener for restart button
-// $('.fa-repeat').on('click', function(event) {
+$('.fa-repeat').on('click', function(event) {
+
+	initializeClock('clockdiv', deadline);
 
 	// startPom();
 
-// });
+});
 
 
 
 // start Pomodoro
-function startPom() {
+// function startPom() {
 
-	startTimer();
+	// startTimer();
 
-}
+// }
