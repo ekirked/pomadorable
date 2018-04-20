4119b4f6adcccec2e56e76f3f17e6ca71150e36b
ekirkegaard <kirkegaard.emily@gmail.com>
2018-04-20 11:59:58 -0700

    re-added python3.6 to virtualenv

--- virtualenv/bin/activate.fish
+++ virtualenv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/Emily/workspace/testing_goat/virtualenv"
+set -gx VIRTUAL_ENV "/Users/Emily/workspace/pomadorable/virtualenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
